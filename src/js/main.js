e3858fb761bc0e4e4b26d315d1ed795bf7d1bcff
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-03-11 12:09:06 -0300

    try week 2

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
+
+listing.init();
+
+// Check if the user has already visited the site by checking for a cookie
+function checkFirstVisit() {
+    if (!getCookie("visited")) {
+        // Show the banner if it's the first visit
+        document.querySelector(".first-banner").style.display = "block";
+        // Set a cookie to remember that the user has visited
+        setCookie("visited", "true", 365);
+    }
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Function to get the value of a cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Call the checkFirstVisit function when the page loads
+window.onload = checkFirstVisit;
